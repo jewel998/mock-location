c5495c99ee2c34ed1532bf9a0a975b12de016f8e
Jewel Barman <jewelbarman998@gmail.com>
2024-09-10 18:06:19 +0530

    Instead of throwing Error in Web Return false

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { MockLocationPlugin, MockLocationResult } from './definitions';
 
 export class MockLocationWeb extends WebPlugin implements MockLocationPlugin {
-  async isMockLocation(options: { whitelist: string[]; }): Promise<MockLocationResult> {
+  async isMockLocation(): Promise<MockLocationResult> {
     return { isEnabled: false };
   }
 
